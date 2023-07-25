cabfdd2f8be29cb79cc3a9752b9c9e786c9c05e0
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-24 22:24:39 -0300

    Add files via upload

---
+++ Single Responsibility/Single_Responsibility/Single_Responsibility/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    public class Producto
+    {
+        public Producto(String descripcion,double precio)
+        {
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public String Descripcion { get; set; }
+        public double Precio { get; set;}
+    }
+}
---
+++ Single Responsibility/Solid_S/Solid_S/Beer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_S
+{
+    public class Beer
+    {
+        public String Name { get; set; }
+        public String Brand { get; set; }
+
+        public int Alcohol { get; set; }
+        public Beer(String name,String brand)
+        {
+            Name = name;
+            Brand = brand;
+        }
+    }
+}
