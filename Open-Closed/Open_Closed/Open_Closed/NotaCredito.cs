79f211dc8179ced1c586e913553af1f4c52b5374
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-24 22:25:24 -0300

    Add files via upload

---
+++ Open-Closed/Open_Closed/Open_Closed/NotaCredito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed
+{
+    public class NotaCredito : DocumentoContable
+    {
+        public NotaCredito(int numero) : base(numero) { }
+
+        public override string Descripcion()
+        {
+            return $"NC N°{Numero}";
+        }
+    }
+}
---
+++ Open-Closed/Open_Closed/Open_Closed/NotaDebito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed
+{
+    public class NotaDebito : DocumentoContable
+    {
+        public NotaDebito(int numero) : base(numero) { }
+
+        public override string Descripcion()
+        {
+            return $"ND N°{Numero}";
+        }
+    }
+}
